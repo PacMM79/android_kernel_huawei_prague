049a81396046609fc077750367f2f28198cca518
Fran Palmero <sweetydesigns@gmail.com>
2018-10-17 03:25:59 +0200

    misc: add fastchg driver
    Control forced fast charge via /sys/kernel/fast_charge/force_fast_charge (0 or 1).
    Signed-off-by: Laster K. <officiallazerl0rd@gmail.com>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -53,8 +53,13 @@ enum msm_usb_phy_type {
 	SNPS_28NM_INTEGRATED_PHY,
 };
 
-#define IDEV_CHG_MAX	1500
+#define IDEV_CHG_MAX	1800
+#define IDEV_CHG_MIN	500
 #define IUNIT		100
+
+#define IDEV_ACA_CHG_MAX	1800
+#define IDEV_ACA_CHG_LIMIT	500
+#define IDEV_HVDCP_CHG_MAX	1800
 
 /**
  * Different states involved in USB charger detection.
